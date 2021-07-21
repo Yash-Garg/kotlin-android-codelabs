bdb871591faedd926fdb6e0865390db0249fae3b
Yash-Garg <yash.garg@outlook.in>
2021-07-21 20:47:51 +0530

    tip_time: completed functionality and handle crash
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- tip_time/app/src/main/java/com/app/tiptime/MainActivity.kt
+++ tip_time/app/src/main/java/com/app/tiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.app.tiptime
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.app.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +12,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // Set initial value of format specifier to $0
+        binding.tipResult.text = getString(R.string.tip_amount_text, "$0.00")
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDoubleOrNull() ?: return
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount_text, formattedTip)
     }
 }
