9d0dca26cdb4801af979bb30cd3bc9550c4cba15
Yash-Garg <yash.garg@outlook.in>
2021-07-22 21:34:48 +0530

    affirmations: implement recycler view with some data
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- affirmations/app/src/main/java/com/app/affirmations/MainActivity.kt
+++ affirmations/app/src/main/java/com/app/affirmations/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.app.affirmations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.app.affirmations.adapter.ItemAdapter
+import com.app.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myDataset = DataSource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        recyclerView.adapter = ItemAdapter(this, myDataset)
     }
 }
---
+++ affirmations/app/src/main/java/com/app/affirmations/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.app.affirmations.model
+
+data class Affirmation(val stringResourceID: Int)
